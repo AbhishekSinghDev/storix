3410752454927dd26b83ef4a7c646817589b9422
AbhishekSinghDev <singhabhishek7689@gmail.com>
2025-03-01 15:48:10 +0530

    runtime error fix for using hook useSidebar outside its wrapper

--- src/app/dashboard/product-branding.tsx
+++ src/app/dashboard/product-branding.tsx
@@ -4,15 +4,11 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-  useSidebar,
 } from "@/components/ui/sidebar";
 import Logo from "@/components/shared/logo";
 import ThemeToggleButton from "@/components/shared/theme-toggle-button";
-import { cn } from "@/lib/utils";
 
 const ProductBranding = () => {
-  const { state } = useSidebar();
-
   return (
     <SidebarMenu>
       <SidebarMenuItem>
@@ -21,9 +17,7 @@ const ProductBranding = () => {
           className="flex items-center justify-between rounded-none data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
         >
           <Logo />
-          <ThemeToggleButton
-            className={cn(state === "collapsed" && "hidden")}
-          />
+          <ThemeToggleButton />
         </SidebarMenuButton>
       </SidebarMenuItem>
     </SidebarMenu>
--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -1,34 +1,21 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-import { useSidebar } from "../ui/sidebar";
 
 type LogoProps = {
   className?: string;
 };
 
 const Logo = ({ className }: LogoProps) => {
-  const { state } = useSidebar();
-
   return (
     <Link
       href="/"
       className={cn(
         "flex items-center gap-1 bg-black text-lg font-semibold dark:bg-background sm:text-xl",
         className,
-        state === "collapsed" &&
-          "w-full justify-center rounded-full bg-black dark:bg-white",
       )}
     >
-      {state === "collapsed" ? (
-        <span className="flex items-center justify-center text-white dark:text-black">
-          S
-        </span>
-      ) : (
-        <>
-          <span className="bg-white px-1 text-black">Storix</span>
-          <span className="text-white dark:px-0">.app</span>
-        </>
-      )}
+      <span className="bg-white px-1 text-black">Storix</span>
+      <span className="text-white dark:px-0">.app</span>
     </Link>
   );
 };
