4eb70ab4fa55bd3753f0bbfecaf1e954b9923ff8
AbhishekSinghDev <singhabhishek7689@gmail.com>
2025-06-18 15:42:34 +0530

    add landing page.

--- apps/storix-web/src/app/page.tsx
+++ apps/storix-web/src/app/page.tsx
@@ -1,23 +1,44 @@
-import { getQueryClient } from "@/lib/query-client";
+import CtaSection from "@/components/landing-page/cta-section";
+import FaqSection from "@/components/landing-page/faq-section";
+import FeaturesSection from "@/components/landing-page/features-section";
+import Footer from "@/components/landing-page/footer";
+import Header from "@/components/landing-page/header";
+import HeroSection from "@/components/landing-page/hero-section";
+import HowItWorksSection from "@/components/landing-page/how-it-works-section";
+import LogosSection from "@/components/landing-page/logos-section";
+import PricingSection from "@/components/landing-page/pricing-section";
+import TestimonialsSection from "@/components/landing-page/testimonials-section";
 
-const page = async () => {
-  const queryClient = getQueryClient();
-  const data = await queryClient.fetchQuery({
-    queryKey: ["test"],
-    queryFn: async () => {
-      return "This is server side fetch response";
-    },
-  });
-
+export default function LandingPage() {
   return (
-    <div>
-      This is server side fetch:{" "}
-      <span className="text-red-500 font-bold">{data}</span>, Adding this to
-      just verify server side react query working perfect and for the normal
-      client side fetching use normal useQuery() from the @tanstack/react-query
-      package
+    <div className="flex flex-col">
+      <Header />
+      <main className="flex-1 mx-auto max-w-screen-xl">
+        {/* Hero Section */}
+        <HeroSection />
+
+        {/* Logos Section */}
+        <LogosSection />
+
+        {/* Features Section */}
+        <FeaturesSection />
+
+        {/* How It Works Section */}
+        <HowItWorksSection />
+
+        {/* Testimonials Section */}
+        <TestimonialsSection />
+
+        {/* Pricing Section */}
+        <PricingSection />
+
+        {/* FAQ Section */}
+        <FaqSection />
+
+        {/* CTA Section */}
+        <CtaSection />
+      </main>
+      <Footer />
     </div>
   );
-};
-
-export default page;
+}
